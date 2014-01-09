cbc5ee40c3efbda0bd0f90ebe82fe1301f0175ab
timduru <timduru@gmail.com>
2014-01-09 14:52:17 +0100

    Battery decrease logs
    Change-Id: Ic4fd43bb153c37d36eeda67c006be07d8b36c8b2

--- services/java/com/android/server/BatteryService.java
+++ services/java/com/android/server/BatteryService.java
@@ -85,7 +85,7 @@ import java.io.PrintWriter;
 public final class BatteryService extends IBatteryService.Stub {
     private static final String TAG = BatteryService.class.getSimpleName();
 
-    private static final boolean DEBUG = true;
+    private static final boolean DEBUG = false;
 
     private static final int BATTERY_SCALE = 100;    // battery capacity is a percentage
 
