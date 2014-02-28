b8375b95b6dcc61f54e8d8a81b94a2b33090edb2
timduru <timduru@gmail.com>
2014-02-28 10:43:33 +0100

    QuickSettings: Add Orientation tile, remove battery tile by default
    Change-Id: I91b4a653104746360c8ab05145ec699f4373a287

--- core/java/org/meerkats/katkiss/QSConstants.java
+++ core/java/org/meerkats/katkiss/QSConstants.java
@@ -34,7 +34,7 @@ public class QSConstants {
             TILES_DEFAULT.add(TILE_BLUETOOTH);
             TILES_DEFAULT.add(TILE_AIRPLANE);
             TILES_DEFAULT.add(TILE_GPS);
-            TILES_DEFAULT.add(TILE_BATTERY);
+            TILES_DEFAULT.add(TILE_AUTOROTATE);
             TILES_DEFAULT.add(TILE_EXPANDEDDESKTOP);
         }
 }
