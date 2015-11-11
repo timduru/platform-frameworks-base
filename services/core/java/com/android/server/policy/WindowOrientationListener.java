fb83a2d184e655d83405d80dd07fc737bd73a37e
timduru <timduru@gmail.com>
2015-11-11 12:53:11 +0100

    Fix AutoRotation
    event.timestamp can be skewed after sleep as it doesn't include sleep time on some platforms
    
    lastTouch is initialised with RTC timestamp
    and as such must be compared with RTC and not with event.timestamp

--- services/core/java/com/android/server/policy/WindowOrientationListener.java
+++ services/core/java/com/android/server/policy/WindowOrientationListener.java
@@ -241,6 +241,8 @@ public abstract class WindowOrientationListener {
     }
 
     abstract class OrientationJudge implements SensorEventListener {
+        protected long mTouchEndedTimestampNanos = Long.MIN_VALUE;
+
         // Number of nanoseconds per millisecond.
         protected static final long NANOS_PER_MS = 1000000;
 
@@ -299,6 +301,11 @@ public abstract class WindowOrientationListener {
 
         @Override
         public abstract void onSensorChanged(SensorEvent event);
+
+       public boolean touchEnded() {
+            final long now = SystemClock.elapsedRealtimeNanos(); // must compare with RTC and not with event.timestamp, event.timestamp can be skewed after sleep as it doesn't include sleep time on some platforms
+            return (now > mTouchEndedTimestampNanos + PROPOSAL_MIN_TIME_SINCE_TOUCH_END_NANOS); 
+        }
     }
 
     /**
@@ -501,7 +508,6 @@ public abstract class WindowOrientationListener {
         private boolean mAccelerating;
 
         // Timestamp when the last touch to the touch screen ended
-        private long mTouchEndedTimestampNanos = Long.MIN_VALUE;
         private boolean mTouched;
 
         // Whether we are locked into an overhead usage mode.
@@ -889,8 +895,7 @@ public abstract class WindowOrientationListener {
             }
 
             // The last touch must have ended sufficiently long ago.
-            if (mTouched || now < mTouchEndedTimestampNanos
-                    + PROPOSAL_MIN_TIME_SINCE_TOUCH_END_NANOS) {
+            if (mTouched || !touchEnded()) {
                 return false;
             }
 
@@ -970,7 +975,6 @@ public abstract class WindowOrientationListener {
 
     final class OrientationSensorJudge extends OrientationJudge {
         private boolean mTouching;
-        private long mTouchEndedTimestampNanos = Long.MIN_VALUE;
         private int mProposedRotation = -1;
         private int mDesiredRotation = -1;
         private boolean mRotationEvaluationScheduled;
@@ -1046,13 +1050,7 @@ public abstract class WindowOrientationListener {
         }
 
         private boolean isDesiredRotationAcceptableLocked(long now) {
-            if (mTouching) {
-                return false;
-            }
-            if (now < mTouchEndedTimestampNanos + PROPOSAL_MIN_TIME_SINCE_TOUCH_END_NANOS) {
-                return false;
-            }
-            return true;
+            return (!mTouching && touchEnded());
         }
 
         private void scheduleRotationEvaluationIfNecessaryLocked(long now) {
