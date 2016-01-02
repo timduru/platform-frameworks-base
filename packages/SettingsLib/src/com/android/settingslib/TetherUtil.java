199e05b4a9158f2592f29c6b5ca611f939f5f769
timduru <timduru@gmail.com>
2016-01-02 15:18:57 +0100

    TetherUtil : test if null
    (Settings => More crash)

--- packages/SettingsLib/src/com/android/settingslib/TetherUtil.java
+++ packages/SettingsLib/src/com/android/settingslib/TetherUtil.java
@@ -66,6 +66,8 @@ public class TetherUtil {
     private static boolean isEntitlementCheckRequired(Context context) {
         final CarrierConfigManager configManager = (CarrierConfigManager) context
              .getSystemService(Context.CARRIER_CONFIG_SERVICE);
+        if(configManager == null) return false;
+        if(configManager.getConfig() == null) return false;
         return configManager.getConfig().getBoolean(CarrierConfigManager
              .KEY_REQUIRE_ENTITLEMENT_CHECKS_BOOL);
     }
